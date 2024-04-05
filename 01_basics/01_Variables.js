68a9421b21b0e54505dc3a5d26e50cafcf4be3e4
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-04-05 07:15:05 +0000

    Variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,6 @@
+const accountId = 1442 //Can not be changed now
+let accountEmail = "umair@email.com"
+var accountPassword = "12345"
+accountCity = "Lalamusa"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
