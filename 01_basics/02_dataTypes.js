748454d7f8919e4c0eee1268e3ae1b979d78ac77
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-04-05 14:57:13 +0000

    data types basics

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat the code as newer version
+//alert // we cant use such thing because we are in node js not in browser
+
+let name = "Umair" //string
+let age = 24 //number
+let isLoggedIn = true //boolean
+
+/*
+number  = 2 to power 53
+bigint = bigger int values
+boolean
+null = standalone value / empty but it type act as object
+undefined = not defined
+symbol = uniqueness
+
+object 
+
+*/
