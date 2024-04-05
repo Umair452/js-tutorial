997fc628cdc30ef62d79a96a376e3b5188e76d15
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-04-05 07:21:49 +0000

    Variables Learned with basics

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,6 @@
 const accountId = 1442 //Can not be changed now
-let accountEmail = "umair@email.com"
-var accountPassword = "12345"
+let accountEmail = "umair@email.com" //can be changed within the scope, should 
+var accountPassword = "12345"//can be changed anywhere 
 accountCity = "Lalamusa"
 
 console.table([accountId, accountEmail, accountPassword, accountCity])
