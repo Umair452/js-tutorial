470c5214d86d4e6ddf8da220bdb9c771935a20c8
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-04-07 10:56:39 +0000

    data type extra info

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,23 @@
+//Primitive data type
+
+//String, Number, Boolean, NUll, undefined, Symbol, BigInt
+
+// const id = Symbol("123");
+// const anotherId = Symbol("123");
+// The above two are not the same because symbo makes them unique
+
+//const number = 383798749743873497943794n; the integer is now big int because of n 
+
+
+//Reference / Non-primitive Arrays, Objects, Functions
+
+const heroes = ["Salman", "Shahrukh", "Amir"];
+let id ={
+    name: "Umair",
+    age: 24,
+}
+
+const myFunction = function (){
+    console.log("First function");
+}
+
