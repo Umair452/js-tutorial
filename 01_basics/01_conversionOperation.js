f5f90bab61d1509a07761c08d77afa9624b8af64
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-04-07 10:28:47 +0000

    data types and operations basic

--- 01_basics/01_conversionOperation.js
+++ 01_basics/01_conversionOperation.js
@@ -20,8 +20,31 @@
 //string will also gives true
 //empty string will give false
 
-let someNumber = 33;
+// let someNumber = 33;
 
-let stringNumber = String(someNumber);
-console.log(typeof (stringNumber));
-console.log(stringNumber);
+// let stringNumber = String(someNumber);
+// console.log(typeof (stringNumber));
+// console.log(stringNumber);
+
+// *******************OPERATIONS*********************************
+
+let value = 3;
+
+let negValue = -value; //converted to negative value
+
+// console.log(negValue);
+
+let str1 = "Hello";
+let str2 = " Umair ";
+
+//console.log(str1 + str2);
+
+// console.log("1" + 2 + 2); // as 1 is string it will deal with as string for other numbers
+// console.log(1 + "2" + 2); // will give same output as above because 2 is considered as string  and the 
+//followers are also considered as string
+
+//console.log(+true); 
+// it will convert it into string and gives 1
+
+
+
