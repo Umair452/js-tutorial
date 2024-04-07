42ce66469d13c4e92bcb0c9aab0a8c0fbb94ad39
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-04-07 10:38:17 +0000

    comparison

---
+++ 01_basics/01_comparison.js
@@ -0,0 +1,15 @@
+// console.log(2 < 1);
+// console.log("2" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+//console.log(null <= 0); this >< will converts the null to 0 first so thats why it is true 
+//and null > 0 is false
+
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0); these all gives false  
+
+//avoid such convrsion by comparing non similar data types
+
