b4be6d4b393b33cd5e721b0e9d331f568760d7e0
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-04-07 10:14:03 +0000

    conversion of data types

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,27 @@
+//let score = "33";
+// console.log(typeof (score));
+// let valueInNumber = Number (score);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+//33 is converted to number
+//33abc will converted to give NAN
+//true will converted to give 1
+//null will converted to give 0 
+//unfedined will converted to NAN
+
+//let numberInput = 1;
+
+//let booleanValue = Boolean(numberInput);
+//console.log(booleanValue);
+
+//1 will give true in boolean 
+//0 will give false
+//string will also gives true
+//empty string will give false
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(typeof (stringNumber));
+console.log(stringNumber);
