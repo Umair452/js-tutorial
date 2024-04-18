a8ee62ce88831af7855a93e7b683fcba8590807b
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-04-18 00:09:36 +0000

    object in detail

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -45,5 +45,76 @@ userData.access = function(){
     console.log(`I accessed my name, ${this["full name"]}`); //this will reference to same obj
 }
 
-console.log(userData.greeting());
-console.log(userData.access());
+// console.log(userData.greeting());
+// console.log(userData.access());
+
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+//OBJECT DESTRUCTURING
+
+const course = {
+    name:"JavaScript",
+    price:"1000",
+    courseInstructor:"Hitesh",
+}
+
+const {courseInstructor: teacher} = course;
+
+// console.log(teacher);
+ //Now we can use teacher instead of long name
+
+//API may occur in json or in array format to handle
