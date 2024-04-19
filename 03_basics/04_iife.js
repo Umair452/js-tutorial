5a944cfbb4ae331b8091abac38f9a857ce8760ff
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-04-19 14:19:07 +0000

    iife function

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,8 @@
+//Immediately invoked function expression
+//When you want to invoke a function as soon as the app is started
+//() just enclose the function inside these brackets
+
+( function starter() {
+    console.log("App started");
+})();
+//always place semicolon after ending iife function to avoid errors in case of 2 iife
