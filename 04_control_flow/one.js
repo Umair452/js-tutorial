e4b5b1bb738acb836e83c85860bb15af0031835a
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-04-20 09:15:15 +0000

    basic if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,67 @@
+//if statement
+// const isLoggedIn = true;
+
+// const temp = 41;
+
+// if (temp < 50) {
+//     console.log("Less then 50");
+// }
+// else{
+//     console.log("Temperature is greater then 50");
+
+// }
+
+// <, >, <=, >=, ==, !=, ===,  !==
+
+// const score = 200;
+
+
+// if(score > 100){
+//     const power = "Fly";
+//     console.log(`User Power: ${power}`);
+// }
+// console.log(`User Power: ${power}`);
+
+//SHorthand method of if
+
+// let balance = 1000;
+
+//Single line shorthand
+// if (balance >500) console.log("Balance Checked Single line");
+
+
+//Multi line shorthand 
+// if (balance >500) console.log("Balance Checked"),
+// console.log("Balance Checked Multi line");
+
+//If else if
+
+// if(balance < 500){
+//         console.log("Blanace less then 500");
+// }
+
+// else if(balance < 750){
+
+//     console.log("Blanace less then 750");
+
+// }
+
+// else {
+//     console.log("Blanace less then 1200");
+    
+// }
+
+// const userLoggedIn = true;
+// const debitCard = true;
+// const googleLogged = false;
+// const emailLogged = true;
+
+// if(userLoggedIn && debitCard){
+//     console.log("Allowed to buy the course");
+// }
+
+// if(googleLogged || emailLogged){
+//     console.log("Allowed to see the course");
+// }
+
+
