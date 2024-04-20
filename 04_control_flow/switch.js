c282814e67655a485a4f3193cb11ad57e70e6259
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-04-20 09:58:14 +0000

    basic switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 1;
+switch (month) {
+    case 1:
+        console.log("Month is january");
+        break;
+    case 2:
+        console.log("Month is febraury");
+        break;
+        
+    case 3:
+        console.log("Month is march");
+        break;
+        
+    case 4:
+        console.log("Month is april");
+        break;
+
+    default:
+        console.log("None of the above ");
+        break;
+}
+
