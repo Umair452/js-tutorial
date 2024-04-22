5795c308617b6904e2530120faf9114a64224c01
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-04-22 06:57:36 +0000

    while and do while

---
+++ 05_loops/02_whileLoop.js
@@ -0,0 +1,30 @@
+// while (condition) {
+    
+// }
+
+// let x = 0;
+
+// while (x<=10) {
+
+//     console.log(`Value of x is: ${x}`);
+//     x++;
+    
+// }
+
+// let myArray = ["Flash", "Batman", "Superman"];
+// let counter = 0;
+// while (counter < myArray.length) {
+
+//     console.log(`${myArray[counter]}`);
+//     counter++;
+    
+// }
+
+
+// let score = 11;
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++;
+// } while (score <= 10);
+
