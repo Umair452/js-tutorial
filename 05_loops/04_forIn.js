c26119de124d51d7308f8a0fd2fa4ed35914b362
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-04-22 07:25:57 +0000

    for in loop

---
+++ 05_loops/04_forIn.js
@@ -0,0 +1,43 @@
+//for In loop
+
+let myObject = {
+    game1: "NFS",
+    game2: "GTA",
+    game3: "PUBG",
+}
+
+// for (const key in myObject) {
+
+// //it will simply print they keys only
+//     //console.log(key); 
+    
+//     //Print the values as well
+
+//     console.log(`${key} is: ${myObject[key]}`);
+    
+// }
+
+
+//Arrays
+
+// let programming = ["C++", "JS", "Python"];
+
+// for (const index in programming) {
+//    console.log(`Index is: ${index} value is: ${programming[index]}`);
+// }
+
+
+// let map = new Map();
+
+// map.set("Key1", "Value one for key1");
+// map.set("Key2", "Value one for key2");
+// map.set("Key3", "Value one for key3");
+// map.set("Key1", "Value one for key1");
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+//Not iteratable with for in
+
+
