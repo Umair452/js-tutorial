012c42a9cd08cbcfd12626c2eab44a47cd87a34b
Umair452 <120240458+Umair452@users.noreply.github.com>
2024-06-23 07:33:37 +0000

    promises in js

--- 07_api_and_promises/promises.js
+++ 07_api_and_promises/promises.js
@@ -52,15 +52,70 @@ const fourthPromise = new Promise(function(resolve,reject){
 
 //how to do chaining of then to get values, keep in mid to use catch because we are also handling rejections as well
 fourthPromise.then((user)=>{
-console.log(user);
+// console.log(user);
 return user.userName
 })
 .then((userName)=>{
-    console.log(userName);
+    // console.log(userName);
 })
 .catch((error)=>{
-    console.log(error);
+    // console.log(error);
 })
-.finally(()=> console.log('Promise is either resolved or rejected'))
+// .finally(()=> console.log('Promise is either resolved or rejected'))
 
 //finally used to know wether task is performed or rejected
+
+
+
+
+//using async await instead of then and catch
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false //this is used to see error is there or not
+        if(!error){
+         resolve({userName: "Umair", password: "123456"})
+        } 
+        else{
+         reject('ERROR: SOMETHING WENT WRONG IN JS')
+        }
+     },1000)
+})
+
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+    console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+    
+}
+
+consumePromiseFive()
+
+
+
+// async function getAllUsers(){
+//    try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await response.json()
+//     console.log(data);
+//    } catch (error) {
+//     console.log('E:', error);
+//    }
+// }
+// getAllUsers()
+
+
+//same above thing with then and catch
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+console.log(data);
+})
+.catch(()=> console.log(error))
+
+//Why fetch is working first and timeout working later
